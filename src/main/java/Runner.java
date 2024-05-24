c6f1c189c930b262ba79e545d1e112ed45709c95
hüseyin bayram <kanka65.08@gmail.com>
2024-05-24 22:23:41 +0300

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,13 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        // version 1
+        System.out.println("hello GIT");
+        System.out.println("git add .: staing areaya alir");
+
+
+
+
+
+    }
+}
